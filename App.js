86bf304a8b5c6b954ec7a6ba82dd89988ac362e2
pedro-candido <pedroferreirac@hotmail.com>
2021-12-20 22:40:40 -0300

    feat: statusbar color changed and greeting added

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { Home } from './src/pages'
 
 const App = () => {
   return (
-    <Home />
+    <>
+      <StatusBar barStyle='light-content' />
+      <Home />
+    </>
   )
 }
 
